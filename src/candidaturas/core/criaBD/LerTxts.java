f899ff41af6ef29cfcc1bb521040413ed0b8cdaa
Flavio Meira <flaviomeira10@gmail.com>
2014-03-03 04:03:34 +0000

    index: Melhorar escolha da função a ser executada (a estrutura está pronta para flexibilizar) Criação do servlet que interage com o front end

--- src/candidaturas/core/criaBD/LerTxts.java
+++ src/candidaturas/core/criaBD/LerTxts.java
@@ -1,6 +1,39 @@
 package candidaturas.core.criaBD;
 
-public class LerTxts {
-	String nomeArquivo;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+//import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LerTxts")
+public class LerTxts extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LerTxts() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		lerReceitaTxt(request, response);
+	}
+	
+	protected void lerReceitaTxt(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//request.getParameter(null);
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><BODY>" + request.getParameter("nomeArquivo") + "</BODY></HTML>");
+		
+	}
 
 }
